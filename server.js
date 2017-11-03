dbc44ddb5073fcdc06a4d02e7736ba3bc6fb507b
unknown <tsdcanavan@yahoo.com>
2017-11-03 18:36:05 -0400

    update and delete work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser');
 var mvr = require('method-override');
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static("public"));
